96a5cb361c986e4a313e45cebb9a8adc76ab389d
dolev goaz <www.dolev@gmail.com>
2024-12-01 23:40:51 +0200

    don't update already parsed months

--- src/excel.py
+++ src/excel.py
@@ -19,7 +19,11 @@ def update_paycheck_log(excel_file_path: str, month: str, paycheck_data: dict[st
         workbook = None
         df = pd.DataFrame()  # If file doesn't exist, create a new DataFrame
     
+    df.columns = pd.to_datetime(df.columns).to_period("M")
     month_period = month_str_to_period(month)
+    if month_period in df.columns:
+        print(f"{month}: Nothing to update")
+        return 
     if month_period not in df.columns:
         df[month_period] = None
     
